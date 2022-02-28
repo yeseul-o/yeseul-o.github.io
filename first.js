d57e5f8fbe20082981ba4ddfeac78f4825a93b6d
yeseul-o <99158577+yeseul-o@users.noreply.github.com>
2022-02-28 14:07:35 -0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+// alert("Have you done all assignmnets?");
+
+$("#gn").draggable();
+
+$("#pill").click(function(){
+$("#jerry").hide();
+});
+
+$("#pill").hover(function(){
+$("#backtoindex").append("zzzzzzzz");
+$("#backtoindex").css("color","pink");
+});
+
+})
